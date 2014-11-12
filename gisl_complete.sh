4b4e0d81ec945caf958c2198f9db307a9b8b10e9
bhenderson <henderson.bj@gmail.com>
2014-11-12 07:58:34 -0800

    Fix completion func to have the proper GIT_DIR
    fixes "not a git dir" error messages.

--- gisl_complete.sh
+++ gisl_complete.sh
@@ -1,3 +1,6 @@
 # copy git completion to gisl
 # /usr/share/bash-completion/bash_completion
-_xfunc git complete -o bashdefault -o default -o nospace -F __git_wrap__git_main gisl
+__gisl_wrap__git_main() {
+  GIT_DIR="$(gisl rev-parse --git-dir)" __git_wrap__git_main "$@"
+}
+_xfunc git complete -o bashdefault -o default -o nospace -F __gisl_wrap__git_main gisl
